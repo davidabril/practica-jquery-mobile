96d5761f85de1fafc2956ee915fb92b08b21ba0f
David Abril <alumnodaw2davidabril@gmail.com>
2015-02-16 00:10:29 +0100

    Añadido main.js con jquery en la carpeta scripts

---
+++ scripts/main.js
@@ -0,0 +1,71 @@
+$("#pagina").on("pageshow" , function() {	
+	/* Cambios en la valoracion del contenido*/
+	$('#nota11').on('change', function() { 
+		$('#notafin1').val(parseFloat($('#nota11').val())
+			+parseFloat($('#nota12').val())
+			+parseFloat($('#nota13').val())
+			);
+		$('.notafinal').html(parseFloat($('#notafin1').val())
+			+parseFloat($('#notafin2').val())
+			+parseFloat($('#notafin3').val())
+			);
+	});
+	$('#nota12').on('change', function() { 
+		$('#notafin1').val(parseFloat($('#nota11').val())
+			+parseFloat($('#nota12').val())
+			+parseFloat($('#nota13').val())
+			);
+		$('.notafinal').html(parseFloat($('#notafin1').val())
+			+parseFloat($('#notafin2').val())
+			+parseFloat($('#notafin3').val())
+			);	
+	});
+	$('#nota13').on('change', function() { 
+		$('#notafin1').val(parseFloat($('#nota11').val())
+			+parseFloat($('#nota12').val())
+			+parseFloat($('#nota13').val())
+			);
+		$('.notafinal').html(parseFloat($('#notafin1').val())
+			+parseFloat($('#notafin2').val())
+			+parseFloat($('#notafin3').val())
+			);	
+	});
+	/* Cambios en la valoracion de calidad*/
+	$('#notafin2').on('change', function() { 
+		$('.notafinal').html(parseFloat($('#notafin1').val())
+			+parseFloat($('#notafin2').val())
+			+parseFloat($('#notafin3').val())
+			);		
+	});
+	/* Cambios en la valoracion de la expresion*/
+	$('#nota31').on('change', function() { 
+		$('#notafin3').val(parseFloat($('#nota31').val())
+			+parseFloat($('#nota32').val())
+			+parseFloat($('#nota33').val())
+			);
+		$('.notafinal').html(parseFloat($('#notafin1').val())
+			+parseFloat($('#notafin2').val())
+			+parseFloat($('#notafin3').val())
+			);	
+	});
+	$('#nota32').on('change', function() { 
+		$('#notafin3').val(parseFloat($('#nota31').val())
+			+parseFloat($('#nota32').val())
+			+parseFloat($('#nota33').val())
+			);
+		$('.notafinal').html(parseFloat($('#notafin1').val())
+			+parseFloat($('#notafin2').val())
+			+parseFloat($('#notafin3').val())
+			);		
+	});
+	$('#nota33').on('change', function() { 
+		$('#notafin3').val(parseFloat($('#nota31').val())
+			+parseFloat($('#nota32').val())
+			+parseFloat($('#nota33').val())
+			);
+		$('.notafinal').html(parseFloat($('#notafin1').val())
+			+parseFloat($('#notafin2').val())
+			+parseFloat($('#notafin3').val())
+			);		
+	});
+});
