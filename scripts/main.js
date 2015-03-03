8f67490e0aad1b3045a300e91403fcf28ab74b16
David Abril <alumnodaw2davidabril@gmail.com>
2015-03-03 21:52:47 +0100

    Cambios js y html

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,45 @@
+/* Comprueba que introduce un usuario y contraseña */
+$("input[type=submit]").click(function(e) {
+    $nom = $("#nombre").val();
+     $pas = $("#password").val();
+        if ($nom == '' || $pas == '') {
+            e.preventDefault();
+            if ($nom == '' && $pas == ''){
+            	alert("Por favor, introduzca usuario y/o contraseña.");
+            }else{
+            	if ($nom == '') {
+	            	alert("Por favor, introduzca un usuario");
+	           	}        		
+	         	if ($pas == '') {
+            		alert("Por favor, introduzca una contraseña.");
+           		}
+            }
+            
+        }else{
+           window.location.href="#alumnos"; 
+        }
+});
+/*Obliga seleccionar curso y alumno*/
+$("#evaluar").click(function(e) {
+        if ($("#selectAlumno").val() == '' || $("#curso").val() == '') {
+            e.preventDefault();
+            alert("Por favor, seleccione un curso y el alumno que va a evaluar.");
+        }else{
+           window.location.href="#page1"; 
+        }
+});
+/* Guarda en la nota final los cambios durante la valoracion de todos los contenidos*/
 $("#pagina").on("pageshow" , function() {	
 	/* Cambios en la valoracion del contenido*/
-	$('#nota11').on('change', function() { 
-		$('#notafin1').val(parseFloat($('#nota11').val())
+    $("input[id*='nota1']").on('change', function() { 
+    	$('#notafin1').val(parseFloat($('#nota11').val())
 			+parseFloat($('#nota12').val())
 			+parseFloat($('#nota13').val())
 			);
 		$('.notafinal').html(parseFloat($('#notafin1').val())
 			+parseFloat($('#notafin2').val())
 			+parseFloat($('#notafin3').val())
-			); 
-	});
-	$('#nota12').on('change', function() { 
-		$('#notafin1').val(parseFloat($('#nota11').val())
-			+parseFloat($('#nota12').val())
-			+parseFloat($('#nota13').val())
-			);
-		$('.notafinal').html(parseFloat($('#notafin1').val())
-			+parseFloat($('#notafin2').val())
-			+parseFloat($('#notafin3').val())
-			);	
-	});
-	$('#nota13').on('change', function() { 
-		$('#notafin1').val(parseFloat($('#nota11').val())
-			+parseFloat($('#nota12').val())
-			+parseFloat($('#nota13').val())
-			);
-		$('.notafinal').html(parseFloat($('#notafin1').val())
-			+parseFloat($('#notafin2').val())
-			+parseFloat($('#notafin3').val())
-			);	
+			);		
 	});
 	/* Cambios en la valoracion de calidad*/
 	$('#notafin2').on('change', function() { 
@@ -38,7 +49,7 @@ $("#pagina").on("pageshow" , function() {
 			);		
 	});
 	/* Cambios en la valoracion de la expresion*/
-	$('#nota31').on('change', function() { 
+	$("input[id*='nota3']").on('change', function() { 	
 		$('#notafin3').val(parseFloat($('#nota31').val())
 			+parseFloat($('#nota32').val())
 			+parseFloat($('#nota33').val())
@@ -47,26 +58,6 @@ $("#pagina").on("pageshow" , function() {
 			+parseFloat($('#notafin2').val())
 			+parseFloat($('#notafin3').val())
 			);	
-	});
-	$('#nota32').on('change', function() { 
-		$('#notafin3').val(parseFloat($('#nota31').val())
-			+parseFloat($('#nota32').val())
-			+parseFloat($('#nota33').val())
-			);
-		$('.notafinal').html(parseFloat($('#notafin1').val())
-			+parseFloat($('#notafin2').val())
-			+parseFloat($('#notafin3').val())
-			);		
-	});
-	$('#nota33').on('change', function() { 
-		$('#notafin3').val(parseFloat($('#nota31').val())
-			+parseFloat($('#nota32').val())
-			+parseFloat($('#nota33').val())
-			);
-		$('.notafinal').html(parseFloat($('#notafin1').val())
-			+parseFloat($('#notafin2').val())
-			+parseFloat($('#notafin3').val())
-			);		
 	});
 });
 
